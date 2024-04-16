033c75e5eac8d979fa86e508947a87ca08f84bf5
gatchang-nyawargak <nyawargakgatchang@gmail.com>
2024-04-16 07:13:05 +0300

    added tuple file

---
+++ tuple.py
@@ -0,0 +1,24 @@
+print(k[1:])
+print(k[:4])
+# checking for membership
+if 10 in k:
+    print("found")
+else:
+    print("not found")
+# Repetition
+print(k*3)
+# finding max
+print(max(k))
+# finding minimum
+print(min(k))
+y=("Ibiza","Nairobi","Juba")
+# concatenating
+print(k+y)
+# checking length
+if len (k)== len(y):
+    print("lenghth is same")
+else:
+    print("Not the same")
+# deleting a tuple
+del k
+print(k)
